073b84339b3fa089ed1467999289c8f3718f5542
Aagaman <aagaman12658@gmail.com>
2024-05-09 14:10:34 +0545

    reached chapter 92

--- moderation/index.js
+++ moderation/index.js
@@ -13,7 +13,7 @@ app.post('/events', async (req, res)=>{
         .includes('orange') ? 'rejected':'approved';
         console.log("reached here");
         console.log(status);
-        await axios.post('http://localhost:4005/events',{
+        await axios.post('http://event-bus-srv:4005/events',{
             type: 'CommentModerated',
             data: {
                 id: data.id,
